be04b1079f123c7882faf863a9d70cedd40615fa
Garrett Grimm <garrett@prudentpublishing.com>
2011-12-29 22:40:50 -0500

    GG added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+setup(name='pymusic',
+	version='1.0',
+	py_modules=['pymusic'],
+	author='Garrett Grimm',
+	author_email='garrett@grimmdude.com'
+	url='http://www.grimmdude.com'
+	)
