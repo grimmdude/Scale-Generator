33b07c9a15c574f418f813c5d54ffc346a5a3e88
grimmdude <garrett@grimmdude.com>
2012-01-20 00:49:27 -0500

    GG remove setup.py for now

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup
-setup(name='pymusic',
-	version='1.0',
-	py_modules=['pymusic'],
-	author='Garrett Grimm',
-	author_email='garrett@grimmdude.com'
-	url='http://www.grimmdude.com'
-	)
